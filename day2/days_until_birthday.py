a0982f9ba6c142ccfec2dbb1e0d0ee5d5789ec37
mz106 <69387856+mz106@users.noreply.github.com>
2021-05-18 21:32:43 +0100

    Update days_until_birthday.py

--- day2/days_until_birthday.py
+++ day2/days_until_birthday.py
@@ -1,1 +1,24 @@
 
+def how_many_days_until_my_birthday():
+
+    today = datetime.now()
+    birthday = input("Please enter your birthday (YYYY-M-D): ")
+    dt = datetime.strptime(birthday, '%Y-%m-%d')
+    
+    strp_today = today.strftime("%j")
+    strp_dt = dt.strftime("%j")
+
+
+
+    total = int(strp_dt) - int(strp_today)
+    str_total = str(total)
+    if total < 0:
+        print("You have gone back in time! Please enter a current or future date.")
+        how_many_days_until_my_birthday()
+    elif str_total == "0":
+        print("Happy Birthday!")    
+    elif str_total == "1":
+        print(f"There is {str_total} day until your birthday")
+    else:
+        print(f"There are {str_total} days until your birthday")
+how_many_days_until_my_birthday()  
